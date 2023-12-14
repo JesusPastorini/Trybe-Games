446e6c945b44767f87c975d2717ba167152f815d
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-12-14 09:11:37 -0300

    req 06

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -30,8 +30,9 @@ public class TrybeGamesDatabase
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
     public List<Game> GetGamesOwnedBy(Player playerEntry)
     {
-        // Implementar
-        throw new NotImplementedException();
+        var gamesOwnedByPlayer = Games.Where(game => game.Players.Contains(playerEntry.Id)).ToList();
+
+        return gamesOwnedByPlayer;
     }
 
 
